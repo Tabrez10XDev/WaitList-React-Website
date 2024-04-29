5dfa55ab4c7339cfbcbb2a59b93160ec4368e544
lowjunkie <meet.tabrezt1@gmail.com>
2024-04-29 20:10:34 +0530

    Finished NFT Anims

--- src/components/NFTBetModal.js
+++ src/components/NFTBetModal.js
@@ -11,6 +11,7 @@ import blueTicket from "../assets/blueTicket.png"
 import groupProfile from "../assets/groupProfile.svg"
 import Marquee from "react-fast-marquee";
 import MarqueeItem from "./MarqueeItem";
+import { motion } from "framer-motion";
 
 
 export default function NFTBetModal({ open, handleClose }) {
@@ -19,9 +20,9 @@ export default function NFTBetModal({ open, handleClose }) {
         return new Promise( res => setTimeout(res, delay) );
     }
 
-    function adjustMarquee(){
-        
-    }
+    const [betX, setBetX] = useState(0)
+
+  
     let marq = 0
 
     const [marqueeValue, setMarqueeValue] = useState(0)
@@ -38,18 +39,15 @@ export default function NFTBetModal({ open, handleClose }) {
         while(marq < 1000){
             setMarqueeValue(curr => curr + 5)      
             marq += 5         
-            await timeout(50)
+            await timeout(30)
         }
         await timeout(1500)
-        console.log(marqueeValue, "here");
-
-
-        await timeout(500)
-        marq = 50
+        
+        marq = 100
         while(marq > 0){
             setMarqueeValue(curr => curr - 2)      
             marq -= 2
-            await timeout(200)
+            await timeout(100)
         }
 
         setMarqueeValue(0)
@@ -95,9 +93,9 @@ export default function NFTBetModal({ open, handleClose }) {
                         </div>
                     </div>
 
-                    <div className="flex">
+                    <div className="flex items-center">
                         <img className="object-contain w-6 h-6 mr-6" src={sound} />
-                        <img className="object-contain w-6 h-6 mr-6" src={close} />
+                        <img onClick={handleClose} className="object-contain w-4 h-4 mr-6 cursor-pointer" src={close} />
 
                     </div>
                 </div>
@@ -127,6 +125,40 @@ export default function NFTBetModal({ open, handleClose }) {
                                 </div>
 
                             </div>
+
+            <div className="flex items-center justify-between mt-14">
+                <div style={{flex:1}}>
+                    
+                </div>
+                <div style={{flex:2}} className="relative items-center justify-center">
+                    <motion.div 
+                    animate={{
+                        left:`${(betX * 25).toString()}%`
+                    }}
+                    style={{
+                        top:-40,
+                    }} className="absolute bg-textBlue py-1.5 px-2.5 rounded-full transform -translate-x-[45%]">
+                            <p className="m-auto font-SatoshiMedium text-[10px] text-white w-16 text-center">{(betX+1)*100}x Upside</p>
+                            <div className="arrow-down-marquee absolute bottom-0 left-1/2 transform translate-y-3/4 -translate-x-1/2"></div>
+                    </motion.div>
+                    <div className="h-1 bg-borderGrey">
+                    <motion.div animate={{width: `${(betX * 25).toString()}%`}}  className="h-1 bg-textBlue"></motion.div>
+                    <div onClick={()=>setBetX(0)} style={{ backgroundColor: betX >= 0 ? "#2071EE" : "#D9D9D9" }} className="h-3 w-3 rounded-full absolute left-0 top-0 transform -translate-y-1/3 cursor-pointer"></div>
+                    <div onClick={()=>setBetX(1)} style={{ backgroundColor: betX >= 1 ? "#2071EE" : "#D9D9D9" }} className="h-3 w-3 bg-textBlue rounded-full absolute left-1/4 top-0 transform -translate-y-1/3 cursor-pointer"></div>
+                    <div onClick={()=>setBetX(2)} style={{ backgroundColor: betX >= 2 ? "#2071EE" : "#D9D9D9" }} className="h-3 w-3 bg-textBlue rounded-full absolute left-2/4 top-0 transform -translate-y-1/3 cursor-pointer"></div>
+                    <div onClick={()=>setBetX(3)} style={{ backgroundColor: betX >= 3 ? "#2071EE" : "#D9D9D9" }} className="h-3 w-3 bg-textBlue rounded-full absolute left-3/4 top-0 transform -translate-y-1/3 cursor-pointer"></div>
+                    <div onClick={()=>setBetX(4)} style={{ backgroundColor: betX >= 4 ? "#2071EE" : "#D9D9D9" }} className="h-3 w-3 bg-textBlue rounded-full absolute left-full top-0 transform -translate-y-1/3 cursor-pointer"></div>
+
+
+                    </div>
+
+                </div>
+                <div style={{flex:1}}>
+                    
+                </div>
+
+            </div>
+
                             <div className="flex items-center justify-between px-5 mt-4">
                                 <div className="w-2/3 py-3 bg-bgBlue rounded-lg cursor-pointer mx-2">
                                     <p className="text-center text-white text-base font-SatoshiBold m-auto">Place Bet</p>
